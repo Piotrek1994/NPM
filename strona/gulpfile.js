b985eb6ba4e9f6a59618959e742d45b39a1cf661
Piorek1994 <pprus94@gmail.com>
2022-04-19 19:09:50 +0200

    Autoprefixer

--- strona/gulpfile.js
+++ strona/gulpfile.js
@@ -1,10 +1,14 @@
 const {src, dest} = require('gulp')
+const cssnano = require('gulp-cssnano')
+const autoprefixer = require('gulp-autoprefixer')
 
 const sass = require('gulp-sass')(require('sass'));
 
 function sassCompiler(done) {
     src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(autoprefixer())
     .pipe(dest('./dist/css'))
     done()
 }
