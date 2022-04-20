cab2047f0c16088673d7a44ac287a79b6635f838
Piorek1994 <pprus94@gmail.com>
2022-04-20 14:58:55 +0200

    Gulp config

--- strona/gulpfile.js
+++ strona/gulpfile.js
@@ -1,16 +1,51 @@
-const {src, dest} = require('gulp')
+const { src, dest, series } = require('gulp')
 const cssnano = require('gulp-cssnano')
 const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const babel = require('gulp-babel')
+const ugligy = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
-const sass = require('gulp-sass')(require('sass'));
-
-function sassCompiler(done) {
-    src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cssnano())
-    .pipe(autoprefixer())
-    .pipe(dest('./dist/css'))
-    done()
+const paths = {
+	sass: './src/sass/**/*.scss',
+	js: './src/js/**/*.js',
+	img: './src/img/*',
+	sassDest: './dist/css',
+	jsDest: './dist/js',
+	imgDest: './dist/img',
 }
 
-exports.default = sassCompiler
+function sassCompiler(done) {
+	src(paths.sass)
+    .pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min',}))
+        .pipe(sourcemaps.write())
+		.pipe(dest(paths.sassDest))
+	done()
+}
+
+function javaScript(done) {
+	src(paths.js)
+    .pipe(sourcemaps.init())
+		.pipe(babel({presets: ['@babel/env'],}))
+		.pipe(ugligy())
+		.pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write())
+		.pipe(dest(paths.jsDest))
+
+	done()
+}
+
+function convertImages(done) {
+	src(paths.img)
+    .pipe(imagemin())
+    .pipe(dest(paths.imgDest))
+	done()
+}
+
+exports.default = series(sassCompiler, javaScript, convertImages)
---
+++ strona/src/js/main.js
@@ -0,0 +1,12 @@
+const test = () => {
+	console.log('ok')
+}
+const test4 = () => {
+	console.log('ok')
+}
+const test3 = () => {
+	console.log('ok')
+}
+const test2 = () => {
+	console.log('ok')
+}
